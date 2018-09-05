159d7a4cc8e5fce41ea6bbf496c366a29162136c
Elliot Robinson <robinse3@erau.edu>
2018-09-05 09:01:24 -0400

    Add initial framework for a Markov model on words

---
+++ MardovWordGenerator/MarkovModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MardovWordGenerator
+{
+    struct FirstCharacter
+    {
+
+    }
+
+    struct MiddleCharacter
+    {
+
+    }
+
+    struct FinalCharacter
+    {
+
+    }
+
+    class MarkovModel
+    {
+        FirstCharacter[] firstCharacters = new FirstCharacter[26];
+        MiddleCharacter[] middleCharacters = new MiddleCharacter[26];
+        FinalCharacter[] finalCharacters = new FinalCharacter[26];
+
+
+        public void AddWord(string word)
+        {
+            if (word.Length > 3)
+            {
+                AddFirstCharacter();
+                AddMiddleCharacters();
+                AddEndCharacter();
+            }
+        }
+
+        private void AddFirstCharacter()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddMiddleCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddEndCharacter()
+        {
+            throw new NotImplementedException();
+        }
+    }
