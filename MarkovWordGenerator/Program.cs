169d3fb67d2a2fe99765ae0c6dad619ff0f4ba0b
Elliot Robinson <robinse3@erau.edu>
2018-09-05 09:36:52 -0400

    Reads words from a file
    Step 2 of #5

--- MarkovWordGenerator/Program.cs
+++ MarkovWordGenerator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace MarkovWordGenerator
     {
         static void Main(string[] args)
         {
+            // The path three directories above the executable directory in VS
+            // From https://stackoverflow.com/a/14549805
+            string wanted_path =
+                Path.GetDirectoryName(Path.GetDirectoryName(
+                    Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())));
+
+            string filepath = $"{wanted_path}\\words_alpha.txt";
+
+            string[] lines = File.ReadAllLines(filepath);
+
+            for (var i = 0; i < 10; ++i)
+            {
+                // use a tab to indent each line of the file.
+                Console.WriteLine("\t" + lines[i]);
+            }
         }
     }
 }
